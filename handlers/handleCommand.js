0c4c386aa271755d6380fe7914845f611297a61e
MikoMIm <m.mordvinow@gmail.com>
2024-05-07 21:20:10 +0200

    Migrated to new server infrastructure. Deleted old server files.

--- handlers/handleCommand.js
+++
@@ -1,7 +0,0 @@
-import executeCommand from '../cli/executeCommand.js';
-
-export const handleCommand = async (line, rl) => {
-    const [command, ...args] = line.trim().split(' ');
-    
-    executeCommand(command, args, rl);
-};
--- server.js
+++ server.js
@@ -1,42 +1,23 @@
 import express from 'express';
 import http from 'http';
-import { Server as SocketIOServer } from 'socket.io';
-import { verifyToken } from './middleware/verifyToken.js';
-import dotenv from 'dotenv';
-import connectDB from './config.js';
-import userRoutes from './routes/userRoutes.js';
-import chatRoutes from './routes/chatRoutes.js';
-import socketHandler from './sockets/socketHandler.js';
+import { Server } from 'socket.io';
+import connectDB from './config/dbсonnection.js';
 
-dotenv.config();
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 connectDB();
 
-const app = express();
-app.use(express.json()); 
-app.use('/users', userRoutes);
-app.use('/chat', chatRoutes);
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something broke!' });
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
-
-const server = http.createServer(app);
-
-const io = new SocketIOServer(server, {
-  cors: {
-    origin: ['https://example.com'], 
-    methods: ['GET', 'POST']
-  }
-});
-
-socketHandler(io); 
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
-console.log("Environment:", process.env.NODE_ENV);
-
-export default app;
